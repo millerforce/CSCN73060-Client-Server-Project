95b05db7ceb76b2cc21d12916c5b6b21fd848662
Connor Miller <miller77fb@gmail.com>
2026-04-06 13:35:20 -0400

    Created ServerSocket.h file

---
+++ Server/ServerSocket.h
@@ -0,0 +1,14 @@
+
+#include <winsock2.h>
+
+class ServerSocket {
+private:
+	int port = -1; // Port to server will listen on
+	SOCKET socket; // Socket server opens for clients to connect to
+	sockaddr_in serverAddress;
+public:
+	ServerSocket(int port);
+	~ServerSocket();
+
+
+};
