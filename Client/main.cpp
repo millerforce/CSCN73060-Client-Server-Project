6e26c59209f095a5811ce2f6502966695c9624ec
Hero <gzoo@uzwelo.com>
2026-04-07 17:17:48 -0400

    rename exception

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,4 +1,15 @@
 
+#include <iostream>
+
+#include "UUID.h"
+#include "file_io.h"
+
 int main(void) {
 
+	std::string telmFile = getRandomTelemFilename();
+
+	std::vector<std::string> telemetryData = readFile("./data/" + telmFile);
+	UUID clientId;
+
+	return 0;
 }
